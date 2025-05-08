02cb7c3e9cc7aaee7a6229f82f99fa06655c6175
niku-co <119169555+niku-co@users.noreply.github.com>
2025-05-08 12:17:22 +0330

    Create Ok("1.1.3")

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NikuAPI.IRepository;
+
+namespace NikuAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VersionController : ControllerBase
+{
+
+    public VersionController()
+    {
+
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<string>> GetVersion()
+    {
+
+        return Ok("1.1.3");
+
+    }
+}
